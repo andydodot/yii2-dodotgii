5434e67b57fea93044569950b663044497b8beb9
henrydewa <henry@finasmart.com>
2015-08-26 14:18:21 +0700

    add model generator

---
+++ model/form.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @var yii\web\View $this
+ * @var yii\widgets\ActiveForm $form
+ * @var yii\gii\generators\form\Generator $generator
+ */
+
+echo $form->field($generator, 'tableName');
+echo $form->field($generator, 'tablePrefix');
+echo $form->field($generator, 'modelClass');
+echo $form->field($generator, 'ns');
+echo $form->field($generator, 'baseClass');
+echo $form->field($generator, 'db');
+echo $form->field($generator, 'generateRelations')->checkbox();
+echo $form->field($generator, 'generateLabelsFromComments')->checkbox();
+echo $form->field($generator, 'generateModelClass')->checkbox();
+<?= "<h4>Timestamp Behaviors</h4>";?>
+<div class="row">
+    <div class="col-md-3">
+<?= $form->field($generator, 'createdAt'); ?>
+    </div>
+    <div class="col-md-3">
+<?= $form->field($generator, 'updatedAt'); ?>
+    </div>
+    <div class="col-md-6">
+<?= $form->field($generator, 'timestampValue'); ?>
+    </div>
+</div>    
+<?php
+echo "<h4>Blameable Behaviors</h4>";
+?>
+<div class="row">
+    <div class="col-md-3">
+<?= $form->field($generator, 'createdBy'); ?>
+    </div>
+    <div class="col-md-3">
+<?= $form->field($generator, 'updatedBy'); ?>
+    </div>
+    <div class="col-md-6">
+<?= $form->field($generator, 'blameableValue'); ?>
+    </div>
+</div>  
+
+echo $form->field($generator, 'enableI18N')->checkbox();
+echo $form->field($generator, 'messageCategory');
